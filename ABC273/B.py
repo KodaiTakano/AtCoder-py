0cec5aa1ec04329588d3ecd7cb1a40cb3dbc9199
KodaiTakano <takano_x_kodai@icloud.com>
2022-11-07 12:08:40 +0900

    ABC273 A,B

---
+++ ABC273/B.py
@@ -0,0 +1,7 @@
+from decimal import Decimal, ROUND_HALF_UP
+
+X, K = map(int, input().split())
+
+for i in range(K):
+    X = int(Decimal(X).quantize(Decimal(f'1E{i+1}'), rounding=ROUND_HALF_UP))
+print(X)
