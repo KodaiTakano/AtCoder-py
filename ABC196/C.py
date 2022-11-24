32d3053b4b0f1f5d7bcbfa409e7559e4ee0176f8
KodaiTakano <takano_x_kodai@icloud.com>
2022-11-24 17:23:38 +0900

    ABC196 C

--- ABC196/C.py
+++ ABC196/C.py
@@ -1,2 +1,11 @@
-N = int(input())
-
+N = input()
+half=len(N)//2
+if len(N)==1:
+    print(0)
+if len(N)%2!=0:
+    N=str(10**(len(N)-1)-1)
+if len(N)%2==0:
+    if int(N[:half])<=int(N[half:]):
+        print(int(N[:half]))
+    else:
+        print(int(N[:half])-1)
