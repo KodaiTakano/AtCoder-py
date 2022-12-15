8bfba3eee6914c75265bdd779b1d1bbba8850aa7
KodaiTakano <takano_x_kodai@icloud.com>
2022-12-15 23:53:59 +0900

    tessoku

---
+++ tessoku/A11.py
@@ -0,0 +1,24 @@
+N, X = map(int, input().split())
+A = list(map(int, input().split()))
+
+def is_ok(mid):
+    # 条件を満たすかどうか？問題ごとに定義
+    return A[mid]>=X
+
+def meguru_bisect(ng, ok):
+    '''
+    初期値のng,okを受け取り,is_okを満たす最小(最大)のokを返す
+    まずis_okを定義すべし
+    ng ok は  とり得る最小の値-1 とり得る最大の値+1
+    最大最小が逆の場合はよしなにひっくり返す
+    '''
+    while (abs(ok - ng) > 1):
+        mid = (ok + ng) // 2
+        if is_ok(mid):
+            ok = mid
+        else:
+            ng = mid
+    return ok
+
+
+print(meguru_bisect(-1, N)+1)
