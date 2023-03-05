793e0e0a06d6e5da92fe2f0d4b26e3f8470be517
KodaiTakano <takano_x_kodai@icloud.com>
2023-03-05 15:29:10 +0900

    ABC292 C

--- ABC292/C.py
+++ ABC292/C.py
@@ -1,25 +1,24 @@
+import math
+
 N = int(input())
 
-def factorization(n):
-    arr = []
-    temp = n
-    for i in range(2, int(-(-n**0.5//1))+1):
-        if temp%i==0:
-            cnt=0
-            while temp%i==0:
-                cnt+=1
-                temp //= i
-            arr.append([i, cnt])
-
-    if temp!=1:
-        arr.append([temp, 1])
-
-    if arr==[]:
-        arr.append([n, 1])
-
-    return arr
-
 ans=0
-# AB=i
-for i in range(1, N):
+for AB in range(1, N):
+    # ABの組み合わせ
+    x=0
+    for A in range(1, int(math.sqrt(AB))+1):
+        if AB%A==0:
+            x+=1
+            if A*A!=AB:
+                x+=1
     
+    y=0
+    CD=N-AB
+    for C in range(1, int(math.sqrt(CD))+1):
+        if CD%C==0:
+            y+=1
+            if C*C!=CD:
+                y+=1
+    
+    ans+=x*y
+print(ans)
