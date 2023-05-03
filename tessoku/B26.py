85d9d2775e5788016738e87f36336d59bcc811fe
KodaiTakano <takano_x_kodai@icloud.com>
2023-05-03 20:51:18 +0900

    tessoku

---
+++ tessoku/B26.py
@@ -0,0 +1,18 @@
+def sieve_of_eratosthenes(x):
+    nums = [i for i in range(x+1)]
+
+    root = int(pow(x,0.5))
+    for i in range(2,root + 1):
+        if nums[i] != 0:
+            for j in range(i, x+1):
+                if i*j >= x+1:
+                    break
+                nums[i*j] = 0
+
+    primes = sorted(list(set(nums)))[2:]
+
+    return primes
+
+N = int(input())
+print(*sieve_of_eratosthenes(N))
+# --> [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
