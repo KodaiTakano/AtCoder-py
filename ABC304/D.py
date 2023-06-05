558ccc2182086b25be693d166b10cf56712c4e80
KodaiTakano <takano_x_kodai@icloud.com>
2023-06-05 18:02:54 +0900

    ABC304 D

--- ABC304/D.py
+++ ABC304/D.py
@@ -9,3 +9,17 @@ a = list(map(int, input().split()))
 B = int(input())
 b = list(map(int, input().split()))
 
+from collections import defaultdict
+import bisect
+
+# d[1000000*A+B]: 上からA番目, 右からB番目のピースにあるいちごの数
+d=defaultdict(int)
+
+for i in range(N):
+    d[1000000*bisect.bisect_left(a, berry[i][0])+bisect.bisect_right(b, berry[i][1])]+=1
+
+
+if len(d)!=(A+1)*(B+1):
+    print(0, max(d.values()))
+else:
+    print(min(d.values()), max(d.values()))
