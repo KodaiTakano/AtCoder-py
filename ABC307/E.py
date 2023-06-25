c7f6b749213c4738b4f1cfae2758a3c18262123f
KodaiTakano <takano_x_kodai@icloud.com>
2023-06-25 20:06:59 +0900

    E

--- ABC307/E.py
+++ ABC307/E.py
@@ -3,22 +3,17 @@ N, M = map(int, input().split())
 MOD = 998244353
 import math
 
-# n,k,n-kが負になる可能性の時は例外処理を書く
-def combinations_count(n, k):
-    return math.factorial(n) // (math.factorial(n - k) * math.factorial(k))
+# dp[i][b]: i人まで見て0と同じ(0)か違うか(1)どうかの時の総数
+dp=[[0]*2 for _ in range(N)]
+dp[0][0]=M
+# print(dp)
 
-if N==2:
-    print(combinations_count(M, 2)%MOD)
-if N==3:
-    ans=1
-    for _ in range(3):
-        ans*=M
-        ans%MOD
-        M-=1
-    print(ans%MOD)
-if N>=4:
-    ans=M
-    for _ in range(N-2):
-        ans*=(M-1)
-        ans%=MOD
-    print((ans*(M-1)%MOD+ans*(M-2)%MOD)%MOD)
+for i in range(1, N):
+    dp[i][0]+=dp[i-1][1]
+    dp[i][1]+=dp[i-1][0]*(M-1)
+    dp[i][1]+=dp[i-1][1]*(M-2)
+    dp[i][0]%=MOD
+    dp[i][1]%=MOD
+print(dp[N-1][1])
+    
+
