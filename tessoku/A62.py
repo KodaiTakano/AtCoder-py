f6b6dbeefa4cf95799a0a2f8fbf0f218b33ff608
KodaiTakano <takano_x_kodai@icloud.com>
2023-07-06 14:09:41 +0900

    tessoku

---
+++ tessoku/A62.py
@@ -0,0 +1,44 @@
+from collections import deque
+from sys import exit
+
+# 入力
+N, M = map(int, input().split())
+G = [[] for _ in range(N)]
+for i in range(M):
+    A, B = map(int, input().split())
+
+    # 頂点 A から頂点 B への辺を張る
+    G[A-1].append(B-1)
+    G[B-1].append(A-1)
+
+# 各頂点が何手目に探索されたか
+# -1 は「まだ探索されていない」ことを表す
+dist = [-1] * N
+
+# todo リストを表すキュー
+que = deque()
+
+# 頂点 0 を始点とする
+dist[0] = 0
+que.append(0)
+
+# キューが空になるまで探索する
+while que:
+    # キューから頂点を取り出す
+    v = que.popleft()
+
+    # 頂点 v から 1 手で行ける頂点 next_v を探索
+    for next_v in G[v]:
+        # 頂点 next_v が探索済みであれば何もしない
+        if dist[next_v] != -1:
+            continue
+
+        # 頂点 next_v を探索する
+        dist[next_v] = dist[v] + 1
+        que.append(next_v)
+
+for d in dist:
+    if d==-1:
+        print("The graph is not connected.")
+        exit()
+print(" The graph is connected.")
