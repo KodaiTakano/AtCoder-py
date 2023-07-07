d6c4ed03eb06b0b22c60b4af052b7ef2f9199455
KodaiTakano <takano_x_kodai@icloud.com>
2023-07-07 20:43:45 +0900

    tessoku

---
+++ tessoku/B62.py
@@ -0,0 +1,55 @@
+from collections import deque
+from sys import exit
+
+# 入力
+N, M = map(int, input().split())
+G = [[] for _ in range(N)]
+for i in range(M):
+    A, B = map(int, input().split())
+
+    # 頂点 A から頂点 B への辺を張る
+    G[A-1].append(B-1)
+    G[B-1].append(A-1)
+
+# 各頂点が何手目に探索されたか
+# -1 は「まだ探索されていない」ことを表す
+dist = [-1] * N
+
+# todo リストを表すキュー
+que = deque()
+
+# 頂点 0 を始点とする
+dist[0] = 0
+que.append(0)
+
+# キューが空になるまで探索する
+while que:
+    # キューから頂点を取り出す
+    v = que.pop()
+
+    # 頂点 v から 1 手で行ける頂点 next_v を探索
+    for next_v in G[v]:
+        # 頂点 next_v が探索済みであれば何もしない
+        if dist[next_v] != -1:
+            continue
+
+        # 頂点 next_v を探索する
+        dist[next_v] = dist[v] + 1
+        que.append(next_v)
+
+# print(dist)
+
+ans=[N-1]
+# ansの末尾から見れる頂点のdistを見て-1であればansに追加
+while 1:
+    v = ans[-1]
+    d = dist[v]
+    if v==0:
+        break
+    for pre_v in G[v]:
+        if dist[pre_v]+1==dist[v]:
+            ans.append(pre_v)
+            break
+ans.reverse()
+for a in ans:
+    print(a+1, sep=" ")
