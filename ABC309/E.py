6a8241213d4d76d9c372e710ed751dcebc0c4cbc
KodaiTakano <takano_x_kodai@icloud.com>
2023-07-09 15:41:55 +0900

    tessoku

--- ABC309/E.py
+++ ABC309/E.py
@@ -7,7 +7,7 @@ from collections import defaultdict
 # 親iが最大何世代先まで保険に入れたか
 d=defaultdict(int)
 for i in range(M):
-    d[A[i][0]] = max(d[A[i][0]], A[i][1])
+    d[A[i][0]-1] = max(d[A[i][0]-1], A[i][1])
 # print(d)
 
 from collections import deque
@@ -21,35 +21,31 @@ for i in range(N-1):
 
 # 各頂点が何手目に探索されたか
 # -1 は「まだ探索されていない」ことを表す
-dist = [-1] * N
+dist = [0] * N
+for k, v in d.items():
+    dist[k]=v+1
+# print(dist)
 
 # todo リストを表すキュー
 que = deque()
 
-for key, value in d.items():
-    # 頂点 0 を始点とする
-    dist[key-1] = 0
-    
-    que.append(key-1)
+que.append(0)
 
-    # キューが空になるまで探索する
-    while que:
-        # キューから頂点を取り出す
-        v = que.popleft()
+# キューが空になるまで探索する
+while que:
+    # キューから頂点を取り出す
+    v = que.popleft()
 
-        if dist[v]+1>value:
-            continue
-
-        # 頂点 v から 1 手で行ける頂点 next_v を探索
-        for next_v in G[v]:
-            # 頂点 next_v を探索する
-            dist[next_v] = dist[v] + 1
-            que.append(next_v)
+    # 頂点 v から 1 手で行ける頂点 next_v を探索
+    for next_v in G[v]:
+        # 頂点 next_v を探索する
+        dist[next_v] = max(dist[next_v], dist[v]-1)
+        que.append(next_v)
 
 # print(dist)
 ans=0
 for i in range(N):
-    if dist[i]!=-1:
+    if dist[i]!=0:
         ans+=1
 
 print(ans)
